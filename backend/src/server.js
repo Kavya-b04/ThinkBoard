3ca459c0c978e398430c0730c8512ad74a6a1168
Kavya-b04 <kavyabalamurugan04@gmail.com>
2025-07-30 13:57:49 +0530

    prepared for deployemt

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,20 +5,25 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js"
 import cors from "cors"
+import path from "path"
 
 
 dotenv.config();
 const app =express();
 const PORT = process.env.PORT || 5001;
-
+const __dirname = path.resolve()
 
 
 
 
 //middleware
-app.use(cors({
+if(process.env.NODE_ENV !== "production")
+{
+    app.use(cors({
     origin:"http://localhost:5173",
 }))
+}
+
 app.use(express.json()); //this midddleware will parse JSON bodies
 app.use(rateLimiter)
 
@@ -30,6 +35,16 @@ app.use(rateLimiter)
 // })
 
 app.use("/api/notes",notesRoutes);
+
+
+if(process.env.NODE_ENV == "production")
+{
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
 
 // app.get("/api/notes",(req,res)=>{
 //     res.send("you send 10 notes");
