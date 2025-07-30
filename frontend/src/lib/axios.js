a04318f5e0efe9e796a98df77154f5fc68f74641
Kavya-b04 <kavyabalamurugan04@gmail.com>
2025-07-30 15:20:28 +0530

    details

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE == "development"?"http://localhost:5001/api":"api"
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://thinkboard-a6ot.onrender.com/api"; // ✅ Absolute path
+
 const api = axios.create({
-    baseURL :BASE_URL,
-})
+  baseURL: BASE_URL,
+});
+
 
 export default api;
